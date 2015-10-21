f948617565c8069a6df1202d85fd1408fb82adb0
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-10-21 14:56:49 +0300

    implemented reading from STDIN and splitting by provided delimiter

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	delimiter = flag.String("d", "", "delimiter substring")
+	part      = flag.Int("p", 0, "part to output (starting from 1)")
+)
+
+func main() {
+	flag.Parse()
+
+	var result []string
+
+	if *delimiter != "" && *part > 0 {
+		stdInBytes, stdInErr := ioutil.ReadAll(os.Stdin)
+		if stdInErr != nil {
+			log.Fatal(errors.New("unable to process STDIN"))
+		}
+		bytesWithoutNewLine := strings.Split(string(stdInBytes), "\n")[0]
+		result = strings.Split(bytesWithoutNewLine, *delimiter)
+		if *part <= len(result) {
+			fmt.Println(result[*part-1])
+		} else {
+			log.Fatal(errors.New("index outside result range"))
+		}
+	} else {
+		log.Fatal(errors.New("both delimiter and part parameters shouldn't be empty"))
+	}
+}
