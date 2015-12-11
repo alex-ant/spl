33e5069da6a2924ef195bb763a615bec0519e420
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-12-12 00:10:58 +0200

    added processing from file + multiple lines

--- main.go
+++ main.go
@@ -15,22 +15,56 @@ var (
 	part      = flag.Int("p", 0, "part to output (starting from 1)")
 )
 
+func process(data []byte) {
+	lines := strings.Split(string(data), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, *delimiter) {
+			result := strings.Split(line, *delimiter)
+			if *part <= len(result) {
+				fmt.Println(result[*part-1])
+			} else {
+				// Printing an empty line if requested part is beyond result's slice.
+				fmt.Println()
+			}
+		} else {
+			// Printing the entire line if it doesn't contain the provided delimiter.
+			fmt.Println(line)
+		}
+		if i == len(lines)-2 {
+			break
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 
-	var result []string
-
 	if *delimiter != "" && *part > 0 {
-		stdInBytes, stdInErr := ioutil.ReadAll(os.Stdin)
-		if stdInErr != nil {
-			log.Fatal(errors.New("unable to process STDIN, " + stdInErr.Error()))
-		}
-		inputWithoutNewLine := strings.Split(string(stdInBytes), "\n")[0]
-		result = strings.Split(inputWithoutNewLine, *delimiter)
-		if *part <= len(result) {
-			fmt.Println(result[*part-1])
+		filePath := flag.Arg(0)
+
+		if filePath != "" {
+			// Processing data from file.
+			stat, fileErr := os.Stat(filePath)
+			switch {
+			case os.IsNotExist(fileErr):
+				log.Fatal(fileErr)
+			case stat.IsDir():
+				log.Fatal(filePath + " is a directory")
+			default:
+				data, dataErr := ioutil.ReadFile(filePath)
+				if dataErr != nil {
+					log.Fatal(dataErr)
+				}
+				process(data)
+			}
 		} else {
-			log.Fatal(errors.New("index outside result's range"))
+			// Processing data from STDIN.
+			data, stdInErr := ioutil.ReadAll(os.Stdin)
+			if stdInErr != nil {
+				log.Fatal(errors.New("unable to process STDIN, " + stdInErr.Error()))
+			}
+			process(data)
 		}
 	} else {
 		log.Fatal(errors.New("both delimiter and part parameters shouldn't be empty"))
